15495816f5048a0e7ed9e901ec23441a764f0476
Rishabh Kumar Gaud <65586827+Rishabh-Gaud@users.noreply.github.com>
2023-09-19 13:47:24 +0530

    task2 50 percent complete

--- controllers/webhook/webhookHandler.js
+++ controllers/webhook/webhookHandler.js
@@ -1,7 +1,6 @@
 const fetchWeatherData = require("../../utils/fetchWeatherData");
 const MsgHook = require("../../whatsapp/MsgHook");
 const sendWaMessage = require("../../whatsapp/sendWaMessage");
-// const axios = require("axios");
 
 const webhookHandler = async (req, res) => {
   const waHookBody = req.body;
@@ -12,56 +11,30 @@ const webhookHandler = async (req, res) => {
   if (MsgHook.isMsg(waHookBody)) {
     const msg = new MsgHook(waHookBody);
     if (msg.hasContext()) {
-      // Example usage:
-      await fetchWeatherData(msg.getMessage())
-        .then((res) => {
-          console.log("res", res);
-          sendWaMessage(
-            msg.getPhoneNumberId(),
-            msg.getFrom(),
-            ` ${msg.getContextMsgFrom()}: ${
-              res === undefined ? "What can we do for you today" : `current temperature in farhen ${res?.main?.temp}` 
-            } `
-          );
-        })
-        .catch((error) => {
-          console.error("Error fetching weather data:", error);
-        });
-      //   const userInput = "WEATHER New York";
-      //   const location = await extractLocationFromText(msg.getMessage());
-      //   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
-      //   const Data = await axios
-      //     .get(apiUrl)
-      //     .then((response) => {
-      //       const weatherData = response.data;
-      //       console.log("Weather Data:", weatherData);
-      //       return weatherData;
-      //     })
-      //     .then((res) => {
-      //       sendWaMessage(
-      //         msg.getPhoneNumberId(),
-      //         msg.getFrom(),
-      //         ` ${msg.getContextMsgFrom()}: ${
-      //           res === null ? "enter correct" : res.main.temp
-      //         } hey we recieved your  reply message.... `
-      //       );
-      //     })
-      //     .catch((error) => {
-      //       console.error("Error fetching weather data:", error);
-      //     });
-      //   console.log(Data);
-      //   await sendWaMessage(
-      //     msg.getPhoneNumberId(),
-      //     msg.getFrom(),
-      //     ` ${msg.getContextMsgFrom()}: ${Data} hey we recieved your  reply message.... `
-      //   );
+      await sendWaMessage(
+        msg.getPhoneNumberId(),
+        msg.getFrom(),
+        "hey we recieved your message...."
+      );
       return res.status(200).end();
     }
-    await sendWaMessage(
-      msg.getPhoneNumberId(),
-      msg.getFrom(),
-      "hey we recieved your message...."
-    );
+    await fetchWeatherData(msg.getMessage())
+      .then((res) => {
+        console.log("res", res);
+        sendWaMessage(
+          msg.getPhoneNumberId(),
+          msg.getFrom(),
+          ` ${
+            res === undefined
+              ? "What can we do for you today"
+              : `current temperature in Kelvin:  ${res?.main?.temp}`
+          } `
+        );
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+      });
+
     return res.status(200).end();
   } else {
     return res.status(403).end();
--- utils/extractLocationFromText.js
+++ utils/extractLocationFromText.js
@@ -1,17 +1,13 @@
 async function extractLocationFromText(inputText) {
-    // Define a regex pattern to match 'WEATHER {location}' format
-    const  pattern = await  /^WEATHER\s+(.+)/i;
-  
-    // Use regex to match and extract the location
-    const match = await inputText.match(pattern);
-  
-    // Check if a match was found and return the location
-    if (match && match[1]) {
-      const location = match[1];
-      return await location.trim(); // Remove leading/trailing spaces
-    } else {
-      return null; // Return null if the format doesn't match
-    }
+  const pattern = await /^WEATHER\s+(.+)/i;
+
+  const match = await inputText.match(pattern);
+  if (match && match[1]) {
+    const location = match[1];
+    return await location.trim(); // Remove leading/trailing spaces
+  } else {
+    return null; // Return null if the format doesn't match
   }
-  
+}
+
 module.exports = extractLocationFromText;
